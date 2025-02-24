7aac218b177f44e4ae74bc9f6738cecff0f3d97a
kobuta23 <gadousinsin@gmail.com>
2025-02-24 14:02:25 -0700

    change contract

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -5,8 +5,8 @@ export const PINATA_JWT = process.env.PINATA_JWT!;
 export const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS! as `0x${string}`;
 export const PRIVATE_KEY = process.env.PRIVATE_KEY! as `0x${string}`;
 export const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN!;
-export const TESTNET:boolean = process.env.TESTNET == 'false' ? false : true;
-export const EXPLORER_URL = `https://${TESTNET ? 'sepolia.' : '' }basescan.org/`;
+export const TESTNET:boolean = process.env.TESTNET == 'FALSE' ? false : true;
+export const EXPLORER_URL = `https://${TESTNET ? 'sepolia.' : '' }basescan.org`;
 export const RPC_URL = `https://base-${TESTNET? 'sepolia' : 'mainnet'}.rpc.x.superfluid.dev/`
 export const MAINNET_RPC_URL = `https://eth-mainnet.rpc.x.superfluid.dev/`;
 export const ADMIN_ID = Number(process.env.ADMIN_ID!);
